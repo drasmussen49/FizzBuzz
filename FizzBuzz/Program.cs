36c123606ef075e27b4bdbbdc2a4100b0c42803d
Dan Rasmussen <drasmussen49@gmail.com>
2020-01-16 16:14:07 -0500

    First commit for this project

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* FizzBuzz myObj = new FizzBuzz();
+            string myNum = myObj.Get(3);
+            Console.WriteLine(myNum);*/
+        }
+    }
+    public class FizzBuzz
+    {
+        public string Get(int num)
+        {
+            if (num == 15)
+            {
+                return "FizzBuzz";
+            }
+            if (IsMultipleOf3(num))
+            {
+                return "Fizz";
+            }
+            if (IsMultipleOf5(num))
+            {
+                return "Buzz";
+            }
+            return num.ToString();
+        }
+
+        private bool IsMultipleOf3(int numberToCheck)
+        {
+            return numberToCheck % 3 == 0;
+        }
+        private bool IsMultipleOf5(int numberToCheck)
+        {
+            return numberToCheck % 5 == 0;
+        }
+    }
+}
